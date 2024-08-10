7dab2a6ab3a084fa58491c8ba0e2a1dc4fddf8c1
Aditya Saini <adityasaini10012001@outlook.com>
2024-08-10 16:22:14 +0530

    some changes
    Signed-off-by: Aditya Saini <adityasaini10012001@outlook.com>

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -1,8 +1,12 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+
 
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI);
+        console.log("MONGO_URI:", process.env.MONGO_URI); 
         console.log('mongodb connected successfully');
     } catch (error) {
         console.log(error);
